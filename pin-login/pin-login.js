fb47b7e85677d46413ca260a8449ac5c33a97a15
Michał Szymański <r0dr1g3zek@gmail.com>
2021-12-22 03:42:32 +0100

    Go

--- pin-login/pin-login.js
+++ pin-login/pin-login.js
@@ -17,16 +17,58 @@ class PinLogin{
             "1", "2", "3",
             "4", "5", "6",
             "7", "8", "9",
-            "backspace", "2", "done",
+            "backspace", "0", "done",
         ];
         padLayout.forEach(key => {
             const insertBreak = key.search(/[369]/) !== -1;
             const keyEL = document.createElement("div");
 
-            keyEL.classList.add("pin-login--key");
+            keyEL.classList.add("pin-login__key");
             keyEL.classList.toggle("material-icons", isNaN(key));
             keyEL.textContent = key;
             keyEL.addEventListener("click", () => {this._handleKeyPress(key)})
+            this.el.numPad.appendChild(keyEL);
+
+            if (insertBreak){
+                this.el.numPad.appendChild(document.createElement("br"));
+            }
         });
+    }
+    _handleKeyPress(key){
+        switch(key){
+            case "backspace":
+                this.value = this.value.substring(0, this.value.length - 1);
+                break;
+                case "done":
+                    this._attemptLogin();
+                    break;
+                    default:
+                        if (this.value.length < this.maxNumber && !isNaN(key)){
+                            this.value += key;
+                        }
+                        break;
+        }
+        this._updateValueText();
+    }
+    _updateValueText(){
+        this.el.textDisplay.value = "_".repeat(this.value.length);
+        this.el.textDisplay.classList.remove("pin-login__text-error");
+    }
+    _attemptLogin(){
+        if(this.value.length > 0){
+            fetch(this.loginEndpoint,{
+                method: "post",
+                headers: {
+                    "Content-type": "application/x-www-form-urlencoded"
+                },
+                body: `pincode=${this.value}`
+            }).then(resposne => {
+                if (resposne.status === 200){
+                    window.location.href = this.redirectTo;
+                } else {
+                    this.el.textDisplay.classList.add("pin-login__text--error");
+                }
+            })
+        }
     }
 }
